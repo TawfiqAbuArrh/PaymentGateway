043c7962a98db84c46519e50522903a8c638fcc7
TawfiqJawwal <Tawfiq.AbuArrh@Jawwal.ps>
2021-03-19 20:03:18 +0200

    Impelement Login API

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -20,15 +20,32 @@ namespace PaymentGateway_Task.Controllers
         [HttpPost]
         public IActionResult post(LoginRequest request)
         {
-            if (request == null || string.IsNullOrEmpty(request.Password) || string.IsNullOrEmpty(request.UserName))
+            try
             {
+                if (request == null || string.IsNullOrEmpty(request.Password) || string.IsNullOrEmpty(request.UserName))
+                {
+                    throw new ArgumentException("Missing parametter");
+                }
+
+                if (_db.Users.Any(s => s.UserName.Equals(request.UserName) && s.Password.Equals(request.Password)))
+                {
+                    var token = Guid.NewGuid();
+                    _db.LoginTokens.Add(new LoginTokens
+                    {
+                        Token = token.ToString(),
+                        TokenExpiration = DateTime.Now.AddMonths(1),
+                        UserId = _db.Users.Where(s => s.UserName == request.UserName).Select(s => s.Id).Single()
+                    });
+                    _db.SaveChanges();
+                    return new OkObjectResult(new { Token = token, Message = "Success Login" });
+                }
+
                 return new UnauthorizedObjectResult(new string("Not Authorized"));
             }
-
-            if (_db.Users.Any(s => s.UserName.Equals(request.UserName) && s.Password.Equals(request.Password)))
-                return new OkObjectResult(new { Token = Guid.NewGuid(), Message = "Success Login" });
-
-            return new UnauthorizedObjectResult(new string("Not Authorized"));
+            catch
+            {
+                return new BadRequestObjectResult(new string("Bad Request"));
+            }
         }
     }
 }
